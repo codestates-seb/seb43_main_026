77c8c07726c0aa9e8097ec8ff739e29fd843d47d
rsuubinn <rsuubinn@gmail.com>
2023-05-02 14:50:09 +0900

    [Feat] 초기 세팅

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

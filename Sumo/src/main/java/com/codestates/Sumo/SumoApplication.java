dfb57a532f5b8f92e1c30279572389167946921b
Seohabin6078 <habin6078@gmail.com>
2023-05-02 11:03:18 +0900

    feat: create project
    기본 프로젝트 파일 생성

---
+++ Sumo/src/main/java/com/codestates/Sumo/SumoApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.Sumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumoApplication.class, args);
+	}
+
+}
---
+++ Sumo/src/test/java/com/codestates/Sumo/SumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.Sumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

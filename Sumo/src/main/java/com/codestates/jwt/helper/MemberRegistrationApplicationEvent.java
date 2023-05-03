a33b4127b71cf6f68473e1440ca7f797cf170b0b
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-05-03 16:29:12 +0900

    feat: AppilcationEvent 생성

---
+++ Sumo/src/main/java/com/codestates/jwt/helper/MemberRegistrationApplicationEvent.java
@@ -0,0 +1,14 @@
+package com.codestates.jwt.helper;
+
+import com.codestates.member.entity.Member;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class MemberRegistrationApplicationEvent extends ApplicationEvent {
+    private Member member;
+    public MemberRegistrationApplicationEvent(Object source, Member member) {
+        super(source);
+        this.member = member;
+    }
+}

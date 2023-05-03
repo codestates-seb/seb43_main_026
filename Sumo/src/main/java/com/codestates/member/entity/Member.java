7932ed3e56127946ff4cca98cb22b232582f5341
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-05-03 15:36:42 +0900

    feat: Member 엔티티에 권한 설정

--- Sumo/src/main/java/com/codestates/member/entity/Member.java
+++ Sumo/src/main/java/com/codestates/member/entity/Member.java
@@ -4,16 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Auditable;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-public class Member {
+public class Member{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
@@ -26,4 +30,12 @@ public class Member {
 
     @Column(nullable = false, unique = true)
     private String nickname;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    public enum MemberRole{
+        ROLE_USER,
+        ROLE_ADMIN
+    }
 }

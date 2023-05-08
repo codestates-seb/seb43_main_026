5b553d805412b431ab122ea7e2c85c8080d6b8a4
rsuubinn <rsuubinn@gmail.com>
2023-05-08 17:34:54 +0900

    feat: footer 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 //리액트 모듈
-import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 //레이아웃
-import Header from "./layout/Header";
-import Footer from "./layout/Header";
-import Nav from "./layout/Nav";
+import Header from './layout/Header';
+import Nav from './layout/Nav';
+import Footer from './layout/Footer';
 
 function App() {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 // import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "./style/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import theme from "./style/theme";
-
-// eslint-disable-next-line import/no-unresolved
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyle from './style/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-
 import theme from './style/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

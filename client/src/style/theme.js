949124ee4f12bb99f264b46462719f30b79a94d6
kijiwon <kiji1313@gmail.com>
2023-05-08 11:07:16 +0900

    style:전역 컬러 변수 추가

--- client/src/style/theme.js
+++ client/src/style/theme.js
@@ -15,8 +15,19 @@ const widthSize = {
 };
 const color = {
   bg: '#FFFFFF',
-  headerBg: '#F8F9F9',
-  footerBg: '#242629',
+  main_blue: `#eff5ff`,
+  main_dark_blue: `#2C599B`,
+  main_gray: `#f2f3f7`,
+  bg_light_blue: `#F0F9FF`,
+  bg_blue: `rgba(228, 235, 244, 1)`,
+  bg_dark: `rgba(67, 67, 67, 0.5)`,
+  bg_place: `#c8d3e1`,
+  bg_comment: `rgba(201, 214, 233, 0.5)`,
+  bg_comment_btn: `C8D3E1`,
+  font_comment: `#696969`,
+  font_post_title: `#434446`,
+  font_forget_pw: `#2592FF`,
+  pc_bg: `#FAFAFA`,
 };
 
 const theme = {

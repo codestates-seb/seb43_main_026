023a9a1123b043aaf803d7d5f98580153b232f53
chochojj <csgy114@gmail.com>
2023-05-08 17:17:35 +0900

    feat: 네브바 헤더 연결 전

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 // import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "./style/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import theme from "./style/theme";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyle from './style/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <ThemeProvider theme={theme}>

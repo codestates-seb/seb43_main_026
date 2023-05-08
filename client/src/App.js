b44d364053806999d62483437ff34cc88b1b3103
kijiwon <kiji1313@gmail.com>
2023-05-08 15:22:02 +0900

    feat:header구현 시작

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+// eslint-disable-next-line import/no-unresolved
 import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-unresolved
+import Header from './layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,5 +1,14 @@
+// eslint-disable-next-line import/no-unresolved
+import styled from 'styled-components';
+
+const HeaderCon = styled.header`
+  width: 100%;
+  height: 50px;
+  background-color: ${(props) => props.theme.color.bg};
+  border: 1px solid black;
+`;
 const Header = () => {
-  return <></>;
+  return <HeaderCon></HeaderCon>;
 };
 
 export default Header;

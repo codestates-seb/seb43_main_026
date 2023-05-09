9f321fc34f73506f288b9495125a6abfb6122495
chochojj <csgy114@gmail.com>
2023-05-09 11:15:55 +0900

    feat: 보드 뼈대 구성

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,4 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.section``;
+const Title = styled.article``;
+const SortBox = styled.article``;
+const UploadBox = styled.article``;
+const ListBox = styled.article``;
+
 const Board = () => {
-  return <></>;
+  return <Container>
+    <Title></Title>
+    <SortBox></SortBox>
+    <UploadBox></UploadBox>
+    <ListBox></ListBox>
+    </Container>;
 };
+
 export default Board;

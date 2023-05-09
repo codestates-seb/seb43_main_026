4cc1e3bf7fd4ab0c19b493a9aa9a5c7f7a786c47
rsuubinn <rsuubinn@gmail.com>
2023-05-09 16:19:06 +0900

    feat: 회원가입 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './layout/Header';
 // import Footer from './layout/Footer';
 import Nav from './layout/Nav';
-import MyCalendar from './pages/Calendar';
-import Board from './pages/Board';
+// import MyCalendar from './pages/Calendar';
+// import Board from './pages/Board';
 
 // 페이지
 import SignUp from './pages/SignUp';

53b5be7cabf21710484afda368bea319032b850a
mins <m2insapp@gmail.com>
2023-05-09 13:55:32 +0900

    comment entity

--- Sumo/src/main/java/com/codestates/comment/entity/Comment.java
+++ Sumo/src/main/java/com/codestates/comment/entity/Comment.java
@@ -1,4 +1,33 @@
 package com.codestates.comment.entity;
 
-public class Comment {
+
+import com.codestates.audit.Auditable;
+import com.codestates.board.entity.Board;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Comment extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long commentId;
+
+    @Column
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "Board_id")
+    private Board board;
+
+
+
+
 }

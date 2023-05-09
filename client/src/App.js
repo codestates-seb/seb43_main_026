31d02bce4dd47729840bf7f3f689455a8e574740
rsuubinn <rsuubinn@gmail.com>
2023-05-09 10:11:16 +0900

    feat: 회원가입 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 //리액트 모듈
 // import { useState } from "react";
 import { BrowserRouter } from 'react-router-dom';
+
 //레이아웃
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 import Nav from './layout/Nav';
+
+// 페이지
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
         <Header />
         <Nav />
         <Footer />
+        <SignUp />
       </BrowserRouter>
     </div>
   );
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,63 @@
+import styled from 'styled-components';
+import LogoImg from '../assets/image/logo.png';
+// import { useForm } from 'react-hook-form';
+
 const SignUp = () => {
-  return <></>;
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm();
+  // const onSubmit = () => {};
+  // const onError = () => {};
+  return (
+    <Container>
+      <div>
+        <Logo src={LogoImg} alt="logo" />
+        <h1>로그인</h1>
+        {/* <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <Input
+            type="text"
+            {...register('nickname', {
+              required: true,
+              minLength: {
+                value: 2,
+                message: '닉네임은 2글자 이상이어야 합니다.',
+              },
+            })}
+          />
+        </form> */}
+      </div>
+    </Container>
+  );
 };
 export default SignUp;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding-top: 50px;
+  background-color: ${(props) => props.theme.color.bg_light_blue};
+  & > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    form {
+      margin-top: 4rem;
+      width: 100vw;
+      height: 100%;
+      border-top-right-radius: 40px;
+      border-top-left-radius: 40px;
+      background-color: white;
+      border: 1px solid ${(props) => props.theme.color.main_blue};
+    }
+  }
+`;
+
+const Logo = styled.img`
+  width: 10rem;
+`;
+
+// const Input = styled.input``;

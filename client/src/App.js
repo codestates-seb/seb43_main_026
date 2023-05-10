9a5bd72fde577504f7c8d7081d57342859ce310b
rsuubinn <rsuubinn@gmail.com>
2023-05-10 14:49:27 +0900

    feat: 로그인 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 //리액트 모듈
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //레이아웃
 import Header from './layout/Header';
-// import Footer from './layout/Footer';
-import Nav from './layout/Nav';
-// import MyCalendar from './pages/Calendar';
-// import Board from './pages/Board';
+import Footer from './layout/Footer';
+// import Nav from './layout/Nav';
 
 // 페이지
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import MyCalendar from './pages/Calendar';
+import Board from './pages/Board';
 
 function App() {
   const [nav, setNav] = useState(false);
@@ -21,13 +22,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header handleNav={handleNav} />
-        <Nav nav={nav} />
-        {/* <Routes>
+        {/* <Nav nav={nav} /> */}
+        <Routes>
           <Route path="/" element={<MyCalendar />} />
           <Route path="/board" element={<Board />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <Footer /> */}
-        <SignUp />
+        <Footer />
       </BrowserRouter>
     </div>
   );
